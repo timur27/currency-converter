a2d4461a012b5ba398a10a5bd6863e0113ab3714
timurk <timur.karimow.95@gmail.com>
2022-01-08 17:09:22 +0100

    Minor

--- application/services/Converter.py
+++ application/services/Converter.py
@@ -1,7 +1,7 @@
 class Converter:
     def __init__(self):
-        self.fxRates = {('EUR', 'USD'): 1.8}
+        self.fx_rates = {('EUR', 'USD'): 1.8}
 
     def convert(self, input, currency, target_currency):
-        rate = self.fxRates[(currency, target_currency)]
+        rate = self.fx_rates[(currency, target_currency)]
         return rate * input
